7cebfe78a90124dc369da58c5a0c20a45e4bcd01
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-14 20:54:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,10 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
+  elsif int % 3 == 0
    return "Fizz"  
   elsif int % 5 == 0 
     return "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0  
-    return "FizzBuzz"
   else
     nil
 end
